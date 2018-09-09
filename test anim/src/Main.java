a1cdb5eaede979b0056ad85d1a11cda506e25784
DuvoisinNicolas <37442663+DuvoisinNicolas@users.noreply.github.com>
2018-09-09 22:58:17 +0200

    Add files via upload

--- test anim/src/Main.java
+++ test anim/src/Main.java
@@ -1,14 +1,11 @@
-import javafx.animation.FadeTransition;
 import javafx.animation.PathTransition;
 import javafx.animation.Timeline;
 import javafx.application.Application;
 import javafx.scene.Scene;
+import javafx.scene.layout.BorderPane;
 import javafx.scene.layout.StackPane;
 import javafx.scene.paint.Color;
-import javafx.scene.shape.CubicCurveTo;
-import javafx.scene.shape.MoveTo;
-import javafx.scene.shape.Path;
-import javafx.scene.shape.Rectangle;
+import javafx.scene.shape.*;
 import javafx.stage.Stage;
 import javafx.util.Duration;
 
@@ -20,28 +17,40 @@ public class Main extends Application {
     @Override
     public void start(Stage primaryStage) {
         primaryStage.setTitle("Hello World!");
-        StackPane root = new StackPane();
-        primaryStage.setScene(new Scene(root, 300, 250));
+        BorderPane root = new BorderPane();
+        primaryStage.setScene(new Scene(root, 800, 600));
+
+        Circle c1 = new Circle(0,0,5);
+
 
         final Rectangle rectPath = new Rectangle (0, 0, 40, 40);
         rectPath.setArcHeight(10);
         rectPath.setArcWidth(10);
         rectPath.setFill(Color.ORANGE);
 
+
         Path path = new Path();
-        path.getElements().add(new MoveTo(20,20));
-        path.getElements().add(new CubicCurveTo(380, 0, 380, 120, 200, 120));
-        path.getElements().add(new CubicCurveTo(0, 120, 0, 240, 380, 240));
+        path.getElements().add (new MoveTo (0, 0));
+        path.getElements().add (new LineTo (500,0));
+        path.getElements().add (new LineTo (500,500));
+
+
         PathTransition pathTransition = new PathTransition();
         pathTransition.setDuration(Duration.millis(4000));
         pathTransition.setPath(path);
         pathTransition.setNode(rectPath);
-        pathTransition.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
         pathTransition.setCycleCount(Timeline.INDEFINITE);
         pathTransition.setAutoReverse(true);
         pathTransition.play();
 
         root.getChildren().add(rectPath);
+
+        root.getChildren().add(c1);
+
         primaryStage.show();
     }
 }
+
+
+
+
