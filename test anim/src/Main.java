772db823e8cbfe5468950c7c20b9351c1c875e5d
DuvoisinNicolas <37442663+DuvoisinNicolas@users.noreply.github.com>
2018-09-09 22:16:02 +0200

    Add files via upload

---
+++ test anim/src/Main.java
@@ -0,0 +1,47 @@
+import javafx.animation.FadeTransition;
+import javafx.animation.PathTransition;
+import javafx.animation.Timeline;
+import javafx.application.Application;
+import javafx.scene.Scene;
+import javafx.scene.layout.StackPane;
+import javafx.scene.paint.Color;
+import javafx.scene.shape.CubicCurveTo;
+import javafx.scene.shape.MoveTo;
+import javafx.scene.shape.Path;
+import javafx.scene.shape.Rectangle;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+
+public class Main extends Application {
+    public static void main(String[] args) {
+        launch(args);
+    }
+
+    @Override
+    public void start(Stage primaryStage) {
+        primaryStage.setTitle("Hello World!");
+        StackPane root = new StackPane();
+        primaryStage.setScene(new Scene(root, 300, 250));
+
+        final Rectangle rectPath = new Rectangle (0, 0, 40, 40);
+        rectPath.setArcHeight(10);
+        rectPath.setArcWidth(10);
+        rectPath.setFill(Color.ORANGE);
+
+        Path path = new Path();
+        path.getElements().add(new MoveTo(20,20));
+        path.getElements().add(new CubicCurveTo(380, 0, 380, 120, 200, 120));
+        path.getElements().add(new CubicCurveTo(0, 120, 0, 240, 380, 240));
+        PathTransition pathTransition = new PathTransition();
+        pathTransition.setDuration(Duration.millis(4000));
+        pathTransition.setPath(path);
+        pathTransition.setNode(rectPath);
+        pathTransition.setOrientation(PathTransition.OrientationType.ORTHOGONAL_TO_TANGENT);
+        pathTransition.setCycleCount(Timeline.INDEFINITE);
+        pathTransition.setAutoReverse(true);
+        pathTransition.play();
+
+        root.getChildren().add(rectPath);
+        primaryStage.show();
+    }
+}
